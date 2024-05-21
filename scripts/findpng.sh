2a98bb4a88da4bea79a8b5065ce232b4bacd9ed6
Xing.Wu <329106954@qq.com>
2024-05-21 21:59:04 +0800

    feat:test

--- scripts/findpng.sh
+++ scripts/findpng.sh
@@ -5,6 +5,7 @@ pwd
 
 # 使用find命令在当前目录及其子目录中查找所有的.png文件  
 # find . -type f -name "*.png" -print  > 
+
 # 初始化一个空数组来存储文件路径  
 png_files=()  
   
@@ -12,16 +13,19 @@ png_files=()
 while IFS= read -r -d '' file; do  
     png_files+=("$file")  
 done < <(find . -type f -name "*.png" -print0)  
+
+echo "数组的元素为: ${my_array[@]}"
+
   
 # 使用jq来构建JSON数组，并将结果写入到文件中  
 #jq -n --args "${png_files[@]}" '{"files": $ARGS.positional}' > png_files.json
 
 # 如果不想使用jq的fromentries，可以简单地创建一个只包含路径的数组  
-jq -n --args "${png_files[@]}" 'args.positional | { "files": . }' > png_files.json
+# jq -n --args "${png_files[@]}" 'args.positional | { "files": . }' > png_files.json
 
 echo "----------------------------分割线----------------------------"
 
-cat ./png_files.json
+# cat ./png_files.json
 
 echo "----------------------------分割线----------------------------"
 
