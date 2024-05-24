ca92759d651babbc05fc15e7c05b199020b21c27
xygodcyx <1323943635@qq.com>
2024-05-24 22:14:59 +0800

    web

---
+++ web/js/data.js
@@ -0,0 +1,340 @@
+const data = {
+	home: [
+		{
+			pic: './asset/1716458856474.png',
+			tip: '测试图片~',
+			url: '#',
+			name: '宝石',
+		},
+	],
+};
+
+const config = {
+	file: [
+		{
+			obj: [
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+				{
+					pic: './asset/1716458856474.png',
+					tip: 'obj',
+					url: '#',
+					name: 'obj',
+				},
+			],
+		},
+		{
+			fpx: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+			],
+		},
+		{
+			fpx: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+			],
+		},
+		{
+			fpx: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+			],
+		},
+		{
+			fpx: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'fpx',
+					url: '#',
+					name: 'fpx',
+				},
+			],
+		},
+	],
+	app: [
+		{
+			blender: [
+				{
+					pic: 'asset/1716515799621.png',
+					tip: 'blender',
+					url: '#',
+					name: 'blender',
+				},
+				{
+					pic: 'asset/1716515799621.png',
+					tip: 'blender',
+					url: '#',
+					name: 'blender',
+				},
+			],
+			company: 'blender',
+		},
+	],
+	company: [
+		{
+			google: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'google',
+					url: '#',
+					name: 'google',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'google',
+					url: '#',
+					name: 'google',
+				},
+			],
+		},
+		{
+			microsoft: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+			],
+		},
+		{
+			microsoft: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+			],
+		},
+		{
+			microsoft: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+			],
+		},
+		{
+			microsoft: [
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+				{
+					pic: 'asset/1716515782501.png',
+					tip: 'microsoft',
+					url: '#',
+					name: 'microsoft',
+				},
+			],
+		},
+	],
+};
