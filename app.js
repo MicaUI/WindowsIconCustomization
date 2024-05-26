f403760aa52be82a175a7041138d5f6a3d1482af
xygodcyx <1323943635@qq.com>
2024-05-26 15:06:10 +0800

    去重、去多尺寸图标

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
 const fs = require('fs');
 const path = require('path');
 
+function excludeDifferentExtensions(data) {
+	for (const key in data) {
+		if (key === '_files') {
+			const filenames = new Set();
+			data[key] = data[key].filter((file) => {
+				const filename = file.name;
+				if (filenames.has(filename)) {
+					return false; // 如果已经存在相同文件名，则排除该文件
+				} else {
+					filenames.add(filename);
+					return true;
+				}
+			});
+		} else if (typeof data[key] === 'object') {
+			data[key] = excludeDifferentExtensions(data[key]); // 递归处理嵌套结构
+		}
+	}
+
+	return data; // 返回处理后的数据
+}
+
+const noCreateFileName = [
+	'16w',
+	'20w',
+	'32w',
+	'40w',
+	'48w',
+	'60w',
+	'64w',
+	'72w',
+	'80w',
+	'96w',
+	'256w',
+	'16',
+	'20',
+	'32',
+	'40',
+	'48',
+	'60',
+	'64',
+	'72',
+	'80',
+	'96',
+	'256',
+];
+function containsKeyword(fileName) {
+	for (let i = 0; i < noCreateFileName.length; i++) {
+		if (fileName.includes(noCreateFileName[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+
 // 函数用于遍历目录下的所有文件
 function traverseDirectory(dir, rootDir) {
 	const companies = {};
@@ -28,11 +82,13 @@ function traverseDirectory(dir, rootDir) {
 				if (!companies['_files']) {
 					companies['_files'] = [];
 				}
-				companies['_files'].push({
-					path: relativePath,
-					name: item.split('.')[0],
-					type: item.split('.')[1],
-				});
+				// Au16w Au20w Au32w
+				if (!containsKeyword(item.split('.')[0]))
+					companies['_files'].push({
+						path: relativePath,
+						name: item.split('.')[0],
+						type: item.split('.')[1],
+					});
 			}
 		}
 	}
@@ -42,8 +98,8 @@ function traverseDirectory(dir, rootDir) {
 
 // 遍历当前目录
 const rootDir = __dirname;
-const result = traverseDirectory(rootDir, rootDir);
-
+let result = traverseDirectory(rootDir, rootDir);
+result = excludeDifferentExtensions(result);
 // 将结果写入config.json文件
 const outputDir = __dirname;
 if (!fs.existsSync(outputDir)) {
