0676c6b92464e8f19e010d8584eb11912cef853d
xygodcyx <xygodcyx@users.noreply.github.com>
2024-05-27 11:14:44 +0000

    deploy: 8083598429e31a493d6c2f902d9474c2fa313de2

--- web/js/index.js
+++ web/js/index.js
@@ -15,6 +15,81 @@ let allAppIconItemData;
 let type;
 let curPage = 'home';
 let noShowBackBtnPage = ['home', 'folders', 'file', 'company', 'software'];
+
+const lightColors = {
+	inputBackgroundColor: '#ffffff',
+	inputBorderColor: '#00000035',
+	inputLabelColor: '#000000',
+
+	leftNavBackground: '#f3f3f3',
+	leftNavItemHoverBackgroundColor: '#00000017',
+	leftCurNavItemBackgroundColor: '#00000017',
+	leftCurNavItemHoverBackgroundColor: '#00000017',
+
+	label: '#000000',
+	icon: '#000000',
+	title: 'rgb(159, 151, 139)',
+
+	mainContentBackground: '#f3f3f3',
+	mainContentLeftTitle: '#000000',
+	mainContentLeftSum: '#7a7979',
+	backBtnColor: '#000000',
+	mainContentIconWrapContent: '#ffffff',
+	mainContentIconWrapContentBottomInfoNameCOlor: '#000000',
+	mainContentIconWrapContentBottomInfoSumCOlor: '#000000',
+
+	showInfoBackgroundColor: 'rgba(255, 255, 255, 0.82)',
+	showInfoBorderColor: '#00000042',
+	showInfoLabelBorderColor: '#00000038',
+	showInfoLabelColor: '#000000',
+	closeColor: '#000000',
+	maskColor: '#2b2b2b',
+};
+
+const darkColors = {
+	inputBackgroundColor: '#ffffff0d',
+	inputBorderColor: '#ffffff14',
+	inputLabelColor: 'rgba(255, 255, 255, 0.786)',
+
+	leftNavBackground: '#202020',
+	leftNavItemHoverBackgroundColor: 'rgba(255, 255, 255, 0.0605)',
+	leftCurNavItemBackgroundColor: 'rgba(255, 255, 255, 0.0605)',
+	leftCurNavItemHoverBackgroundColor: 'rgba(255, 255, 255, 0.0605)',
+
+	label: '#FFFFFF',
+	icon: '#FFFFFF',
+	title: 'rgb(159, 151, 139)',
+
+	mainContentBackground: '#202020',
+	mainContentLeftTitle: '#ffffff',
+	mainContentLeftSum: '#ffffff',
+	backBtnColor: '#ffffff',
+	mainContentIconWrapContent: '#2b2b2b',
+	mainContentIconWrapContentBottomInfoNameCOlor: '#ffffff',
+	mainContentIconWrapContentBottomInfoSumCOlor: '#ffffff',
+
+	showInfoBackgroundColor: '#2c2c2cf0',
+	showInfoBorderColor: '#ffffff0d',
+	showInfoLabelBorderColor: '#ffffff17',
+	showInfoLabelColor: '#FFFFFF',
+	closeColor: '#FFFFFF',
+	maskColor: '#2b2b2b',
+};
+
+// auto change light dark
+const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeMediaQuery.addEventListener('change', (e) => {
+	toggleTheme(e.matches);
+});
+toggleTheme(darkModeMediaQuery.matches);
+function toggleTheme(dark = false) {
+	const colors = dark ? darkColors : lightColors;
+	Object.keys(colors).forEach((key) => {
+		document.documentElement.style.setProperty(`--${key}`, colors[key]);
+	});
+	return dark ? 'dark' : 'light';
+}
+
 const baseUrl = 'https://micaui.github.io/WindowsIconCustomization/';
 const deconstructionFileData = (file) => {
 	return {
@@ -213,6 +288,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		doms.sum.style.opacity = 1;
 		// createIconWrapElement(iconData);
 		createHomePage();
+		setCurStatus(doms.mainHome);
 	});
 });
 let curLeftNavStatus = true;
@@ -607,6 +683,12 @@ const showInfoDataInit = {
 	company: '',
 	type: [],
 };
+
+const showInfoCapitalCaseMap = {
+	app: 'App',
+	company: 'Company',
+	type: 'Type',
+};
 let showInfoData = showInfoDataInit;
 const extractDataToShowInfo = (infoData, showInfoData) => {
 	for (const key in showInfoData) {
@@ -669,7 +751,7 @@ const showShowInfo = (infoData) => {
 		}
 		const infoItem = document.createElement('li');
 		infoItem.classList.add('infoItem');
-		infoItem.innerHTML = `<div class="what" title="${key}">${key}</div>
+		infoItem.innerHTML = `<div class="what" title="${showInfoCapitalCaseMap[key]}">${showInfoCapitalCaseMap[key]}</div>
                         <div class="info" title="${showInfoData[key]}">${showInfoData[key]}</div>`;
 		if (key === 'company' || key === 'app') {
 			addEventListenerInfoItemClickWillGo(
